d2c780826d47dd6735c9979f64f64a534d5adc11
alexander7dvorak <alexander7dvorak@gmail.com>
2022-12-12 14:50:08 +0200

    Removed unnecessary annotation(deprecated for this project after removing EmbeddedMongo dependency).

--- src/test/java/com/drivedynamics/tripservice/AplicationIntegrationTest.java
+++ src/test/java/com/drivedynamics/tripservice/AplicationIntegrationTest.java
@@ -1,15 +1,12 @@
 package com.drivedynamics.tripservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = EmbeddedMongoAutoConfiguration.class)
 class AplicationIntegrationTest {
 
     @Test
