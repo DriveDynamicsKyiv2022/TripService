2a5b0422ad247603027ae310568cb6699030456b
damski-ugodnik <ponomarenkonikita03@gmail.com>
2023-01-31 16:45:39 +0200

    added config for sqs

--- src/main/java/com/drivedynamics/tripservice/config/SqsConfig.java
+++ src/main/java/com/drivedynamics/tripservice/config/SqsConfig.java
@@ -1,23 +1,13 @@
 package com.drivedynamics.tripservice.config;
 
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 public class SqsConfig {
     @Bean
-    @Profile("!local")
     public AmazonSQS amazonSQS() {
         return AmazonSQSClientBuilder.defaultClient();
     }
 
-    @Bean
-    @Profile("local")
-    public AmazonSQS localAmazonSQS() {
-        return AmazonSQSClientBuilder.standard()
-                .withCredentials(new EnvironmentVariableCredentialsProvider())
-                .build();
-    }
 }
