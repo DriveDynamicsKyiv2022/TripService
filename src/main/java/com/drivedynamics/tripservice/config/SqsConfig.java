0c9e56d47f9215d8aeba018439b96e94f279c1e5
damski-ugodnik <ponomarenkonikita03@gmail.com>
2023-01-31 16:48:11 +0200

    forgotten to add config annotation

--- src/main/java/com/drivedynamics/tripservice/config/SqsConfig.java
+++ src/main/java/com/drivedynamics/tripservice/config/SqsConfig.java
@@ -1,13 +1,25 @@
 package com.drivedynamics.tripservice.config;
 
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Configuration
 public class SqsConfig {
     @Bean
+    @Profile("!local")
     public AmazonSQS amazonSQS() {
         return AmazonSQSClientBuilder.defaultClient();
     }
 
+    @Bean
+    @Profile("local")
+    public AmazonSQS localAmazonSQS() {
+        return AmazonSQSClientBuilder.standard()
+                .withCredentials(new EnvironmentVariableCredentialsProvider())
+                .build();
+    }
 }
