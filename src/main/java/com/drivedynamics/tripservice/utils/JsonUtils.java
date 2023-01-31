83c029072ca3473f5792dc104653f89ed8531333
damski-ugodnik <ponomarenkonikita03@gmail.com>
2023-01-31 16:58:14 +0200

    added java time module

--- src/main/java/com/drivedynamics/tripservice/utils/JsonUtils.java
+++ src/main/java/com/drivedynamics/tripservice/utils/JsonUtils.java
@@ -2,9 +2,10 @@ package com.drivedynamics.tripservice.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonUtils {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
     public static <T> T fromJson(String stringRepresentation, Class<T> clazz) {
         try {
